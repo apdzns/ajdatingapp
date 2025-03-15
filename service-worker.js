a3521075a64ec6fc5fa10b7332bd9c6f105d9df0
apdzns <apdzns3@gmail.com>
2025-03-14 21:16:45 -0700

    v2

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,7 @@ const urlsToCache = [
     '/index.html',
     '/styles.css',
     '/script.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
-    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
 ];
 
 self.addEventListener('install', event => {
